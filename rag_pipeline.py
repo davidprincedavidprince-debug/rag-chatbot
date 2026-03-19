c67a862ebb0870306252987a9638cf29e42e4535
Prince David Kirubakaran <David.princedavid.prince@gmail.com>
2026-03-19 17:58:48 +0530

    fix: langchain text splitter import

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -5,7 +5,7 @@ import glob
 import pandas as pd
 
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.schema import Document
