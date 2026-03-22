754e4b5c7e061286218dc234798414291ae5c816
Prince David Kirubakaran <David.princedavid.prince@gmail.com>
2026-03-22 18:25:20 +0530

    fixes: model name and pydantic V2

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -290,7 +290,7 @@ def get_embeddings() -> HuggingFaceEmbeddings:
         model_kwargs["token"] = token
 
     embeddings = HuggingFaceEmbeddings(
-        model_name="all-MiniLM-L6-v2",
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
         model_kwargs=model_kwargs,
         encode_kwargs = {"normalize_embeddings": True}
     )
